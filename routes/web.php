07d205334a35962b80b448414bced0327e5db229
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2026-06-15 23:40:19 +0600

    feat: add user registration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,10 @@ Route::post('/chirps', [ChirpController::class, 'store']);
 Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+// Authentication routes
+Route::view("/register", 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+Route::post("/register", Register::class)
+    ->middleware('guest');
